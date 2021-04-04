d29d1faff3de70dc848623ffe504133e59b2cbdf
Matt Fine <17680862+chewrocca@users.noreply.github.com>
2021-04-04 12:50:56 -0500

    adding install.sh for testing.

--- .install.sh
+++ .install.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 rsync --recursive --verbose --exclude '.git' ~/tmpdotfiles/ $HOME/ \
-  && rm -r ~/tmpdotfiles \
-  && vim +PlugUpgrade +qall \
-  && vim +PlugUpdate +qall \
-  && vim +CocInstall +qall
+  && rm -r ~/tmpdotfiles
